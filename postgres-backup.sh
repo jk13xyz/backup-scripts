2c7c96a51fcaf8d604970dc93facd2790c2b0831
Jens Krieger <jens@jk13.dev>
2024-04-07 18:15:38 +0200

    POSTGRES_USER is checked, if no user passed, script will now default to "postgres"

--- postgres-backup.sh
+++ postgres-backup.sh
@@ -6,18 +6,35 @@ TIMESTAMP=$(date +"%Y%m%d%H%M")
 
 CONTAINER=$(docker ps --format '{{.Names}}:{{.Image}}' | grep 'postgres' | cut -d":" -f1)
 
-echo -e "Start $TIMESTAMP Backup for Databases: \n"
 if [ ! -d $BACKUPDIR ]; then
+    echo -e "Creating backup directory:\n $BACKUPDIR\n"
     mkdir -p $BACKUPDIR
 fi
 
+echo -e "Start $TIMESTAMP Backup for Databases: \n"
+
 for i in $CONTAINER; do
+
+    DATABASE_USER_ENV=$(docker exec $i env | grep -E 'POSTGRES_USER' | cut -d"=" -f1)
+
+    case "$DATABASE_USER_ENV" in
+        POSTGRES_USER)
+            DATABASE_USER=$(docker exec $i printenv POSTGRES_USER)
+            DATABASE_PASSWORD=$(docker exec $i printenv POSTGRES_PASSWORD)
+            ;;            
+        *)
+            DATABASE_USER="postgres"
+            DATABASE_PASSWORD=$(docker exec $i printenv POSTGRES_PASSWORD)    
+        
+            ;;
+    esac
+
     POSTGRES_PASSWORD=$(docker exec $i env | grep POSTGRES_PASSWORD | cut -d"=" -f2)
 
     if docker exec $i test -e /usr/bin/pg_dump; then
         DATABASES=$(docker exec -e PGPASSWORD=$POSTGRES_PASSWORD $i psql -U postgres -lqt | cut -d \| -f 1 | grep -vE 'template[01]|postgres')
         for PG_DATABASE in $DATABASES; do
-            echo -e "Creating PostgreSQL backup for database on container:\n  * $PG_DATABASE DB on $i";
+            echo -e "Creating PostgreSQL backup for database on container:\n  * $PG_DATABASE DB on $i \n";
             docker exec -e PGPASSWORD=$POSTGRES_PASSWORD $i /usr/bin/pg_dump -U postgres $PG_DATABASE | gzip > $BACKUPDIR/$i-$PG_DATABASE-$TIMESTAMP.sql.gz
         done
     else
@@ -30,4 +47,4 @@ for i in $CONTAINER; do
     fi
 done
 
-echo -e "\n$TIMESTAMP Backup for Databases completed\n"
+echo -e "Backup for Databases completed\n"
